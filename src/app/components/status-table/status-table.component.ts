38f414db5ef8bba9bf658d36fd40114636839515
Kjaer <kjaerkjaer@protonmail.com>
2022-12-27 17:55:30 +0100

    Lint fix

--- src/app/components/status-table/status-table.component.ts
+++ src/app/components/status-table/status-table.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { PokemonStatInterface } from 'src/app/interfaces/PokemonStat.interface';
 
 @Component({
-  selector: 'pokemon-status-table',
+  selector: 'app-pokemon-status-table',
   templateUrl: './status-table.component.html',
   styleUrls: ['./status-table.component.scss'],
 })
