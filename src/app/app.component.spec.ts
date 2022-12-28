899624f5ea9e7672c7b53f8a67ee208ff3cb5a8d
Kjaer <kjaerkjaer@protonmail.com>
2022-12-28 16:27:06 +0100

    Adapt test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material-module';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MaterialModule],
+      imports: [RouterTestingModule, MaterialModule, TranslateModule.forRoot()],
       declarations: [AppComponent],
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
