35dd38e75826b7429b61785aa0a2e7d2c9483a84
Kjaer <kjaerkjaer@protonmail.com>
2022-12-28 18:04:07 +0100

    Add shiny sprites

--- src/app/interfaces/PokemonSprite.interface.ts
+++ src/app/interfaces/PokemonSprite.interface.ts
@@ -2,6 +2,8 @@ export interface PokemonSprite {
   other: { [key: string]: any };
   back_default: string;
   front_default: string;
+  front_shiny: string;
+  back_shiny: string;
 
   [key: string]: unknown;
 }
