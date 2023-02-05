c7dbbca6722909b7c458c47aafc4fe6f897a5d90
Kjaer <kjaerkjaer@protonmail.com>
2023-02-05 19:58:04 +0100

    Add update script

---
+++ scripts/loadStaticData.mjs
@@ -0,0 +1,55 @@
+import * as fs from "fs";
+import path from "path";
+import fetch from "node-fetch";
+
+const files = [
+  "versions",
+  "version_names",
+  "version_groups"
+];
+const githubFilesUrl =
+  "https://raw.githubusercontent.com/PokeAPI/pokeapi/master/data/v2/csv/";
+const STATIC_FILES_DIRECTORY = path
+  .join(process.cwd(), "src/assets/data/")
+  .replace(/\\/gm, "/");
+
+function csvJSON(csv) {
+  const lines = csv.split("\n");
+  let result = [];
+  const headers = lines[0].split(",");
+  for (let i = 1; i < lines.length; i++) {
+    let obj = {};
+    const currentline = lines[i].split(",");
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+    result.push(obj);
+  }
+  return result;
+}
+
+function loadFiles() {
+  for (const file of files) {
+    fetch(githubFilesUrl + file + ".csv", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Could not load ${file}`);
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log("\x1b[32m%s\x1b[0m", `✔ ${file} succesfully loaded`);
+        fs.writeFileSync(
+          STATIC_FILES_DIRECTORY + file + ".json",
+          JSON.stringify(csvJSON(data))
+        );
+      })
+      .catch((error) => {
+        console.log("\x1b[31m%s\x1b[0m", "Error:", error);
+      });
+  }
+}
+
+loadFiles();
