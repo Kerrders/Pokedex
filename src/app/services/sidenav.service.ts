b7fa2e806094101e2e65ac4faa06b78749af0ddc
Kjaer <kjaerkjaer@protonmail.com>
2023-11-13 15:41:53 +0100

    FIX redirect on tab closing

--- src/app/services/sidenav.service.ts
+++ src/app/services/sidenav.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { PokemonSpeciesName } from '../interfaces/PokemonSpeciesName.interface';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SidenavService {
+  constructor(private _router: Router) {}
+
   public nodes: Array<{ name: Array<PokemonSpeciesName>; url: string }> = [];
   private _maxNodes = 10;
 
   public removeNode(name: Array<PokemonSpeciesName>): void {
     this.nodes = this.nodes.filter((node) => node.name !== name);
+    this._router.navigate(['']);
   }
 
   public addNode(name: Array<PokemonSpeciesName>, url: string): void {
