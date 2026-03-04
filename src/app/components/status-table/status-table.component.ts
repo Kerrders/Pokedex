933af6779eb325f6328748cca55b2937845611fe
Kjaer <kjaerkjaer@protonmail.com>
2026-03-04 21:56:18 +0100

    run angular migrations

--- src/app/components/status-table/status-table.component.ts
+++ src/app/components/status-table/status-table.component.ts
@@ -3,18 +3,17 @@ import { MatTableModule } from '@angular/material/table';
 import { PokemonStat } from 'src/app/interfaces/PokemonStat.interface';
 import { ColorForStatusValuePipe } from '../../pipes/color-for-status-value.pipe';
 import { TranslateModule } from '@ngx-translate/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-pokemon-status-table',
   templateUrl: './status-table.component.html',
   styleUrls: ['./status-table.component.scss'],
   imports: [
-    CommonModule,
     MatTableModule,
     TranslateModule,
-    ColorForStatusValuePipe,
-  ],
+    ColorForStatusValuePipe
+],
 })
 export class StatusTableComponent {
   public pokemonStats = input<Array<PokemonStat>>([]);
